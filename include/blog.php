c7ab4fd267f9bc2fca03da99275463400339bfe7
Nguyen Louis <nguyenlouis172@gmail.com>
2024-12-18 20:47:07 +0100

    add blog && details

---
+++ include/blog.php
@@ -0,0 +1,25 @@
+<h2>Blog</h2>
+
+<article>
+    <h3>Title 1</h3>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores corporis, nam nostrum at eaque facilis, ipsum ducimus dignissimos maiores harum magni expedita sunt tempora quam, vel earum inventore nisi magnam!
+        Ipsam deserunt reiciendis ea magnam assumenda quos saepe aspernatur dicta delectus perspiciatis voluptatem necessitatibus, non corporis. Voluptatibus corporis repellat, aspernatur, exercitationem eligendi ratione reprehenderit dolor velit optio blanditiis, sequi eum!
+        Tempora magni modi incidunt sapiente repellat libero, laborum minima amet? Officiis inventore consequatur a. Quisquam labore placeat quod consectetur commodi sint at iste, recusandae ea ipsum eveniet aliquam, consequuntur quibusdam!
+    </p>
+
+</article>
+<article>
+    <h3>Title 2</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores corporis, nam nostrum at eaque facilis, ipsum ducimus dignissimos maiores harum magni expedita sunt tempora quam, vel earum inventore nisi magnam!
+        Ipsam deserunt reiciendis ea magnam assumenda quos saepe aspernatur dicta delectus perspiciatis voluptatem necessitatibus, non corporis. Voluptatibus corporis repellat, aspernatur, exercitationem eligendi ratione reprehenderit dolor velit optio blanditiis, sequi eum!
+        Tempora magni modi incidunt sapiente repellat libero, laborum minima amet? Officiis inventore consequatur a. Quisquam labore placeat quod consectetur commodi sint at iste, recusandae ea ipsum eveniet aliquam, consequuntur quibusdam!</p>
+</article>
+<article>
+    <h3>Title 3</h3>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores corporis, nam nostrum at eaque facilis, ipsum ducimus dignissimos maiores harum magni expedita sunt tempora quam, vel earum inventore nisi magnam!
+        Ipsam deserunt reiciendis ea magnam assumenda quos saepe aspernatur dicta delectus perspiciatis voluptatem necessitatibus, non corporis. Voluptatibus corporis repellat, aspernatur, exercitationem eligendi ratione reprehenderit dolor velit optio blanditiis, sequi eum!
+        Tempora magni modi incidunt sapiente repellat libero, laborum minima amet? Officiis inventore consequatur a. Quisquam labore placeat quod consectetur commodi sint at iste, recusandae ea ipsum eveniet aliquam, consequuntur quibusdam!
+    </p>
+</article>
---
+++ include/details.php
@@ -0,0 +1,23 @@
+<h2>Post details</h2>
+<article>
+    <h3>Title 1</h3>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus explicabo voluptatum quidem inventore, cumque eaque tempore minima fugit vitae commodi illum placeat ipsam consectetur facilis nam numquam? Voluptatibus, illum. Reiciendis.
+        Ut consequuntur, perferendis nulla deserunt porro dolorem quidem perspiciatis libero similique maiores, molestias ea, corrupti sequi facilis impedit numquam harum praesentium cumque? Ipsam quis rerum praesentium natus hic nesciunt architecto.
+        Hic tempore, in voluptatem id voluptatibus velit incidunt, odit iure ullam porro inventore eius eveniet, quam officia neque facere rem nemo reprehenderit? Corporis similique explicabo natus! Obcaecati id fuga ab?
+    </p>
+</article>
+
+<section>
+    <h3>Comments</h3>
+    <h3>Comments 1</h3>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae obcaecati earum quae quia laudantium itaque enim reiciendis. Temporibus sed reiciendis ullam a, illo eaque fugit. Inventore, quam. Non, a nulla?
+        Sunt vel consequatur, hic placeat tenetur quo magni illum porro aperiam eum incidunt laboriosam sapiente recusandae? Expedita debitis aliquid sapiente saepe fugit accusamus laborum quisquam, quibusdam odio labore, explicabo id?</p>
+    <h3>Comments 2</h3>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae obcaecati earum quae quia laudantium itaque enim reiciendis. Temporibus sed reiciendis ullam a, illo eaque fugit. Inventore, quam. Non, a nulla?
+        Sunt vel consequatur, hic placeat tenetur quo magni illum porro aperiam eum incidunt laboriosam sapiente recusandae? Expedita debitis aliquid sapiente saepe fugit accusamus laborum quisquam, quibusdam odio labore, explicabo id?</p>
+    <h3>Comments 3</h3>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae obcaecati earum quae quia laudantium itaque enim reiciendis. Temporibus sed reiciendis ullam a, illo eaque fugit. Inventore, quam. Non, a nulla?
+        Sunt vel consequatur, hic placeat tenetur quo magni illum porro aperiam eum incidunt laboriosam sapiente recusandae? Expedita debitis aliquid sapiente saepe fugit accusamus laborum quisquam, quibusdam odio labore, explicabo id?</p>
+
+</section>
