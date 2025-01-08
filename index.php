763b832830d711b1c03dee35fae5d92e6808bd90
Nguyen Louis <nguyenlouis172@gmail.com>
2025-01-08 11:55:08 +0100

    add list categories

--- index.php
+++ index.php
@@ -17,22 +17,32 @@
 
         <?php include 'include/header.php' ?>
         <img src="assets/images/louis.jpg">
-        <?php include 'connexion.php' ?>
+        <?php include 'connexion.php'  ?>
+        <h2>Posts list</h2>
         <?php
         $request = "SELECT * FROM posts";
         $query = mysqli_query($connexion, $request);
         while ($row = mysqli_fetch_array($query)) : ?>
             <h2><?php echo $row['title']; ?></h2>
-            <section id="presentation">
 
-                <p><?php echo $row['content']; ?>
-                </p>
-                <a class="link-button" href="add.php">Add</a>
-                <a class="link-button" href="delete.php?id=<?php echo $row['id']; ?>">Supprimer</a>
-                <a class="link-button" href="update.php?id=<?php echo $row['id']; ?>">Modifier</a>
-                <a class="link-button" href="add_category.php">Add category</a>
-            </section>
+            <p><?php echo $row['content']; ?>
+            </p>
+            <a class="link-button" href="add.php">Add</a>
+            <a class="link-button" href="delete.php?id=<?php echo $row['id']; ?>">Supprimer</a>
+            <a class="link-button" href="update.php?id=<?php echo $row['id']; ?>">Modifier</a>
+            <a class="link-button" href="add_category.php">Add category</a>
+
         <?php endwhile ?>
+        <h2>List categories</h2>
+        <?php
+        $request = "SELECT * FROM category";
+        $query = mysqli_query($connexion, $request);
+        while ($row = mysqli_fetch_array($query)) : ?>
+            <h3><?php echo $row['category_name']; ?></h3>
+
+            <a class="link-button" href="add_category.php">Add category</a>
+        <?php endwhile ?>
+
 
     </main>
 
